e7a0cc7b132cd507547d85dba148fb40df5bfd49
cavichia <cavichia@Oscars-iMac.local>
2021-06-08 11:07:25 -0300

    Apply cache to fibonacci, MUCH faster now

--- fibonacci.py
+++ fibonacci.py
@@ -1,3 +1,7 @@
+from functools import lru_cache
+
+@lru_cache(maxsize=None)
+
 def fibonacci(n):
     if n == 0: 
         return 0
@@ -6,4 +10,4 @@ def fibonacci(n):
     return fibonacci(n - 1) + fibonacci(n - 2)
 
 if __name__ == '__main__':
-    print(fibonacci(20))
+    print(fibonacci(35))
